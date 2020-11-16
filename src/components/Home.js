6febec37ab86e8a941c51a3a7a47d1814c616000
pepelancheros <pepe.lancheros@gmail.com>
2020-11-15 20:40:15 -0500

    products chocolates and empaques

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ import imgBannerFade from '../imgs/fade_banner.png'
 import chocolateMelt from '../imgs/chocolate_melt.svg'
 import chocolatePlate from '../imgs/chocolateplate.png'
 import chocolateInfo from '../imgs/chocolate-info.JPG'
+import chocolatesForestMain from '../imgs/chocolates_forest_main.png'
+import chocolatesForestBackground from '../imgs/chocolates_forest_background.png'
+import blackBoxGif from '../imgs/blackbox.gif'
 
 const Home = () => {
     return (
@@ -29,10 +32,19 @@ const Home = () => {
                     <p className="chocolate-customization__info__text">Personaliza tus chocolates con el porcentaje de cacao, relleno y decoración que más te guste </p>
                 </div>
             </div>
-            <div className="d-flex align-items-center justify-content-center home-products">
-                <div className="horizontal-line"/>
-                <h4 className="text-medium">nuestros productos</h4>
-                <div className="horizontal-line"/>
+            <div className="products">
+                <div className="d-flex align-items-center justify-content-center products__title">
+                    <div className="horizontal-line"/>
+                    <h4 className="text-medium">nuestros productos</h4>
+                    <div className="horizontal-line"/>
+                </div>
+                <div className="products__chocolates">
+                    <img className="products__chocolates__background" src={ chocolatesForestBackground } alt="multiple chocolates picture background"/>
+                    <img className="products__chocolates__main" src={ chocolatesForestMain } alt="multiple chocolates picture"/>
+                </div>
+                <div className="products__empaques">
+                    <img src={ blackBoxGif } alt=""/>
+                </div>
             </div>
         </div>
     )

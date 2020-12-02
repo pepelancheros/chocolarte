3f65c747572c9f86b8ea35586e6c3143e63039eb
pepelancheros <pepe.lancheros@gmail.com>
2020-12-01 21:36:56 -0500

    html and styles of whole homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,9 @@ import chocolateInfo from '../imgs/chocolate-info.JPG'
 import chocolatesForestMain from '../imgs/chocolates_forest_main.png'
 import chocolatesForestBackground from '../imgs/chocolates_forest_background.png'
 import blackBoxGif from '../imgs/blackbox.gif'
+import chocolateBars from '../imgs/chocolatebars.png'
+import chocolateSpecial from '../imgs/witchheel.png'
+import contactBackground from '../imgs/contact-background.JPG'
 
 const Home = () => {
     return (
@@ -35,16 +38,34 @@ const Home = () => {
             <div className="products">
                 <div className="d-flex align-items-center justify-content-center products__title">
                     <div className="horizontal-line"/>
-                    <h4 className="text-medium">nuestros productos</h4>
+                    <h3 className="text-medium">nuestros productos</h3>
                     <div className="horizontal-line"/>
                 </div>
                 <div className="products__chocolates">
+                    <h4 className="text-large products__chocolates__text">Chocolates</h4>
                     <img className="products__chocolates__background" src={ chocolatesForestBackground } alt="multiple chocolates picture background"/>
                     <img className="products__chocolates__main" src={ chocolatesForestMain } alt="multiple chocolates picture"/>
                 </div>
                 <div className="products__empaques">
-                    <img src={ blackBoxGif } alt=""/>
+                    <h4 className="text-large products__empaques__text">Empaques</h4>
+                    <img className="products__empaques__img" src={ blackBoxGif } alt="gif chocolate box opening"/>
                 </div>
+                <div className="products__barras"> 
+                    <img className="products__barras__img" src={ chocolateBars } alt="chocolate bars picture"/>
+                    <h4 className="text-large products__barras__text">Barras</h4>
+                </div>
+                <div className="products__special">
+                    <h4 className="text-large products__special__text">Ocasiones Especiales</h4>
+                    <img className="products__special__img" src={ chocolateSpecial } alt="box of chocolates for halloween with the form of a witch heel"/>
+                </div>
+            </div>
+            <div className="home__contact">
+                <address className="home__contact__text text-large">
+                    Patricia Olarte<br/>
+                    <a href="tel:+573012208727">3012208727</a><br/>
+                    <a href="#">@chocolarte</a>
+                </address>
+                <img className="home__contact__background" src={ contactBackground } alt=""/>
             </div>
         </div>
     )
